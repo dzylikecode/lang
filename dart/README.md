34f3b164040d343ce8e11acd83baaaee6cfa4949
Dz <MathBeWithCode@gmail.com>
2025-12-07 11:16:41 +0800

    example

---
+++ dart/README.md
@@ -0,0 +1,6 @@
+
+```bash
+dart create my_app
+dart pub add xxx
+dart pub remove xxx
+```
---
+++ dart/examples/if_let.dart
@@ -0,0 +1,11 @@
+void main() {
+  if (func() case final a when a > 40) {
+    print('a is greater than 40: $a');
+  } else {
+    print('a is 40 or less: ${func()}');
+  }
+}
+
+int func() {
+  return 42;
+}
